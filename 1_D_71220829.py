79d95b7bfc0fb60c82a704631d3a80c5a4387bf6
TimotiusEkanaT <117902158+TimotiusEkanaT@users.noreply.github.com>
2022-12-01 16:24:35 +0700

    Add files via upload

---
+++ 1_D_71220829.py
@@ -0,0 +1,49 @@
+def ntnn():
+    print ("Dimanakah kamu ingin menonton?")
+    print ("1) XXI Empire")
+    print ("2) XXI Amplaz")
+    print ("3) XXI JCM")
+
+def jjja():
+    print ("Mau nonton film apa nih? Ada film:")
+    print ("1. Frozen")
+    print ("2. Keramat")
+    print ("3. KKN Desa Penari")
+
+def aaas():
+    print("1. Reguler")
+    print("2. Dolby almos")
+    print("3. Premiere")
+
+def adsj():
+    
+def asdh():
+    print("1. 12.35")
+    print("2. 14.45")
+    print("3. 16.55")
+    print("4. 19.05")
+
+def jash():
+    asps=(input("Masukkan angka pilihan Anda: "))
+
+
+ntnn()
+assd=(input("Masukkan pilihanmu: "))
+
+jjja()
+asdhs=(input("Mau nonton di layar bioskop apa:"))
+
+aaas()
+dgfb=(input("Pilih tipe layar:"))
+
+adsj()
+iaus=(input("Berapa banyak tiket?"))
+
+asdh()
+huss=(input("Mau nonton jam berapa:"))
+
+print("Oke berhasil!, silahkan dinikmati di jam",)
+
+
+print("Hallo selamat datang di bioskop!")
+
